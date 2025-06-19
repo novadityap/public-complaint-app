21f94d62693f7cb16d5bda5db435e0d0dcce918b
novadityap <novaaditya23@gmail.com>
2025-06-19 17:50:49 +0700

    Add request classes for category creation, updating, and searching with validation rules

---
+++ server/app/Http/Requests/Category/SearchCategoryRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+          'page' => 'sometimes|integer|min:1',
+          'limit' => 'sometimes|integer|min:1|max:100',
+          'q' => 'sometimes|string'
+        ];
+    }
+}
