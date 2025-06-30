c531f4098ad0332dd366aa884e47c981abbe9968
novadityap <novaaditya23@gmail.com>
2025-06-30 19:46:15 +0700

    Add CloudinaryHelper class with extractPublicId method for URL processing

---
+++ server/app/Helpers/CloudinaryHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Helpers;
+
+class CloudinaryHelper
+{
+  public static function extractPublicId(string $url): ?string
+  {
+    if (strpos($url, 'cloudinary.com') === false) {
+      return null;
+    }
+
+    $parts = explode('/', $url);
+    $uploadIndex = array_search('upload', $parts);
+
+    if ($uploadIndex === false || !isset($parts[$uploadIndex + 2])) {
+      return null;
+    }
+
+    $publicIdParts = array_slice($parts, $uploadIndex + 2);
+    $joined = implode('/', $publicIdParts);
+    $withoutExtension = preg_replace('/\.[^.\s]{3,4}$/', '', $joined);
+
+    return $withoutExtension;
+  }
+}
