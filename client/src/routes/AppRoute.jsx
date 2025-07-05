830c75015757c234618f73b98b431e0aba6e1d00
novadityap <novaaditya23@gmail.com>
2025-07-05 17:45:39 +0700

    Fix import for RequestResetPassword component in AppRoute

--- client/src/routes/AppRoute.jsx
+++ client/src/routes/AppRoute.jsx
@@ -10,7 +10,7 @@ import Signin from '@/pages/public/Signin';
 import Signup from '@/pages/public/Signup';
 import VerifyEmail from '@/pages/public/VerifyEmail';
 import ResendVerification from '@/pages/public/ResendVerification';
-import ResetPasswordRequest from '@/pages/public/ResetPasswordRequest';
+import RequestResetPassword from '@/pages/public/RequestResetPassword';
 import ResetPassword from '@/pages/public/ResetPassword';
 import Unauthorized from '@/pages/public/Unauthorized';
 import NotFound from '@/pages/public/NotFound';
@@ -33,7 +33,7 @@ const router = createBrowserRouter(
         <Route path="resend-verification" element={<ResendVerification />} />
         <Route
           path="request-reset-password"
-          element={<ResetPasswordRequest />}
+          element={<RequestResetPassword />}
         />
         <Route path="reset-password/:resetToken" element={<ResetPassword />} />
         <Route
